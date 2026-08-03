94f061e7bda25c65b99cdc35ab6eefe24c1d64a0
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2026-08-03 11:46:09 -0600

    feat: add agg handler

--- handler_agg.go
+++ handler_agg.go
@@ -1,2 +1,15 @@
 package main
 
+import (
+	"context"
+	"fmt"
+)
+
+func handlerAgg(s *state, cmd command) error {
+	agg, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+	fmt.Println(agg)
+	return nil
+}
--- rss_feed.go
+++ rss_feed.go
@@ -9,6 +9,22 @@ import (
 	"time"
 )
 
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	client := &http.Client{
 		Timeout: time.Second * 10,
