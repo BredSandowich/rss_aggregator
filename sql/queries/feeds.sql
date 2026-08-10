1006f740401ee1d49d0593d87bc6b77654c788fd
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2026-08-10 12:15:05 -0600

    feat: add CreateFeedFollow

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -16,13 +16,20 @@ FROM feeds
 INNER JOIN users
 ON feeds.user_id = users.id;
 
--- name: CreateFeedFollow :many
+-- name: CreateFeedFollow :one
 WITH inserted_feed_follow AS (
-    INSERT INTO feed_follows ...
+    INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+    VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+)
     RETURNING *
 )
+
 SELECT inserted_feed_follow.*, feeds.name AS feed_name, users.name AS user_name
 FROM inserted_feed_follow
-INNER JOIN ...
-INNER JOIN ...
-
+INNER JOIN users ON users.id = inserted_feed_follow.user_id
+INNER JOIN feeds ON feeds.id = inserted_feed_follow.feed_id;
