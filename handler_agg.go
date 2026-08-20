57b2e93c87262edb33f6e1746d92eb042c744665
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2026-08-20 14:22:30 -0600

    feat: add RSS feed aggregation loop

--- handler_agg.go
+++ handler_agg.go
@@ -3,13 +3,57 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+	"errors"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	agg, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	
+	if len(cmd.Args) != 1 {
+		return errors.New("Incorrect arguments entered")
+	}
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
 	if err != nil {
 		return err
 	}
-	fmt.Println(agg)
+
+	fmt.Printf("Collecting feeds every %s\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	defer ticker.Stop()
+
+	for ; ; <-ticker.C {
+		if err := scrapeFeeds(s); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+
+
+func scrapeFeeds(s *state) error {
+	
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return err
+	}
+
+	_, err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		return err
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Println(item.Title)
+	}
+	
 	return nil
 }
